13e741afe954639f85066e3f964c828a4ff894ee
Asterios7 <asterios812@gmail.com>
2023-12-03 23:40:32 +0100

    Create examples.py

---
+++ examples/examples.py
@@ -0,0 +1,24 @@
+from dlib_wrapper import dlibFaceProcessor
+from PIL import Image
+import numpy as np
+
+# Load image and convert to np.ndarray
+img = Image.open("./data/pulp-fiction.jpg")
+img = np.array(img)
+
+# Instantiate face processor
+face_processor = dlibFaceProcessor()
+
+# Image -> Embeddings (option 1)
+# 1. Detect faces, get face boxes
+boxes = face_processor.detect_faces(img)
+# 2. Extract face landmarks
+shapes = face_processor.get_shapes(img, boxes)
+# 3. Align faces
+aligned_faces = face_processor.align_faces(img, shapes)
+# 4. Extract face embeddings
+embeddings1 = face_processor.encode_faces(aligned_faces)
+
+# Image -> Embeddings (option 2)
+# detect_and_encode_faces combines the 4 steps into 1
+embeddings2 = face_processor.detect_and_encode_faces(img)
